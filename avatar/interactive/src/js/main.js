213e3ac5c2beb86e5644022877d244c1dfab0da9
tenjoufire <tenjoufire7@gmail.com>
2023-11-29 11:10:29 +0000

    Update supported languages in main.js

--- avatar/interactive/src/js/main.js
+++ avatar/interactive/src/js/main.js
@@ -24,7 +24,7 @@ let IceServerCredential
 const TalkingAvatarCharacter = "lisa"
 const TalkingAvatarStyle = "casual-sitting"
 
-supported_languages = ["en-US", "de-DE", "zh-CN", "ja-JP"] // The language detection engine supports a maximum of 4 languages
+supported_languages = ["en-US","ja-JP"] // The language detection engine supports a maximum of 4 languages
 
 const BackgroundColor = '#FFFFFFFF'
 
